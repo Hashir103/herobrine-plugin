25b9e663f62094b1c00a9596a68c37d9436b20f7
Hashir103 <hashirsami@hotmail.ca>
2024-06-12 12:33:08 -0400

    citizens dependency
    add

--- src/main/java/org/hashir/herobrine/herobrine.java
+++ src/main/java/org/hashir/herobrine/herobrine.java
@@ -1,15 +1,14 @@
 package org.hashir.herobrine;
-
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class herobrine extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("onEnable is called!");
+        getLogger().info("Herobrine has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("onDisable is called!");
+        getLogger().info("Herobrine has been disabled!");
     }
 }

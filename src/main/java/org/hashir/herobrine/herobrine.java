897ac1efeee4bb9a1420aedfdb3a277e3680a661
Hashir103 <hashirsami@hotmail.ca>
2024-06-26 09:40:08 -0400

    not working

--- src/main/java/org/hashir/herobrine/herobrine.java
+++ src/main/java/org/hashir/herobrine/herobrine.java
@@ -5,6 +5,11 @@ public class herobrine extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Herobrine has been enabled!");
+
+        if (getServer().getPluginManager().getPlugin("Citizens") == null
+        || !getServer().getPluginManager().getPlugin("Citizens").isEnabled()) {
+            getLogger().severe("Citizens 2.0 not found or not enabled. Disabling Herobrine.");
+            }
     }
 
     @Override
